f631053c6d1cee8e9813d5b2627d92fdb760dd62
Jennifer Middleton <frudence@Jennifers-MacBook-Air.local>
2018-09-08 23:58:19 -0400

    GREEN - Project Setup with Spring, Gradle, and Spock

---
+++ src/main/java/com/frudence/talentdemo/TalentdemoApplication.java
@@ -0,0 +1,12 @@
+package com.frudence.talentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentdemoApplication.class, args);
+    }
+}
