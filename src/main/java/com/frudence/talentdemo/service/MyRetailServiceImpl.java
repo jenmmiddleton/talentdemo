13cab38caf75555fb1e189d6ee39f2be6e7800e7
Jennifer Middleton <frudence@Jennifers-MacBook-Air.local>
2018-09-10 21:33:09 -0400

    REFACTOR - Implemented MyRetailService so that the project builds

---
+++ src/main/java/com/frudence/talentdemo/service/MyRetailServiceImpl.java
@@ -0,0 +1,12 @@
+package com.frudence.talentdemo.service;
+
+import com.frudence.talentdemo.model.Product;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyRetailServiceImpl implements MyRetailService {
+    @Override
+    public Product getProduct(Integer id) {
+        return null;
+    }
+}
