b8a406bfdbdaa805631c6bd81ffa097ef33d9900
Jennifer Middleton <frudence@Jennifers-MacBook-Air.local>
2018-09-10 21:45:28 -0400

    RED - GetProduct should call GET request at /products/{id}

--- src/test/groovy/MyRetailControllerTest.groovy
+++ src/test/groovy/MyRetailControllerTest.groovy
@@ -2,6 +2,9 @@ import com.frudence.talentdemo.controller.MyRetailController
 import com.frudence.talentdemo.model.Price
 import com.frudence.talentdemo.model.Product
 import com.frudence.talentdemo.service.ProductService
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import spock.lang.Specification
 import spock.lang.Subject
 
@@ -12,13 +15,17 @@ class MyRetailControllerTest extends Specification {
 
     ProductService productService = Mock()
 
+    MockMvc mockMvc
+
     void setup() {
         myRetailController = new MyRetailController(
                 productService: productService
         )
+
+        mockMvc = MockMvcBuilders.standaloneSetup(myRetailController).build()
     }
 
-    def "GetProduct should call MyRetailService"() {
+    def "GetProduct should call ProductService"() {
         given:
             Integer id = 15117729
             Price price = new Price(
@@ -41,5 +48,30 @@ class MyRetailControllerTest extends Specification {
             response.getId() == id
             1 * productService.getProduct(id) >> product
             response == product
+    }
+
+    def "GetProduct should call GET request at /products/{id}"() {
+        given:
+            Integer id = 15117729
+            Price price = new Price(
+                    id: id,
+                    value: 5.00,
+                    currencyCode: "USD"
+            )
+
+            Product product = new Product(
+                    id: id,
+                    name: "Avatar",
+                    currentPrice: price
+            )
+
+        when:
+            MvcResult mvcResult = mockMvc.perform(get("/products/" + id))
+                                         .andExpect(status().isOk())
+                                         .andReturn()
+
+        then:
+            1 * productService.getProduct(id) >> product
+            mvcResult.getResponse().getContentAsString() == "{\"id\":15117729,\"name\":\"Avatar\",\"current_price\":{\"value\":5.0,\"currency_code\":\"USD\"}}"
     }
 }
