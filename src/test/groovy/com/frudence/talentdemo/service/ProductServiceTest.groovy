d79124ca32877cf3e823257b4a08b10b23b0ea55
Jennifer Middleton <frudence@Jennifers-MacBook-Air.local>
2018-09-15 22:09:59 -0400

    RED - ProductService GetProduct returns a Product

---
+++ src/test/groovy/com/frudence/talentdemo/service/ProductServiceTest.groovy
@@ -0,0 +1,42 @@
+package com.frudence.talentdemo.service
+
+import com.frudence.talentdemo.feign.RedSky
+import com.frudence.talentdemo.model.Price
+import com.frudence.talentdemo.model.Product
+import com.frudence.talentdemo.repository.PriceRepository
+import spock.lang.Specification
+import spock.lang.Subject
+
+class ProductServiceTest extends Specification {
+
+    @Subject
+    ProductService productService
+
+    RedSky redSky = Mock()
+
+    PriceRepository priceRepository = Mock()
+
+    void setup() {
+        productService = new ProductServiceImpl(
+                redSky: redSky,
+                priceRepository: priceRepository
+        )
+    }
+
+    def "GetProduct returns a Product"() {
+        setup:
+            Integer id = 15117729
+            Price price = new Price(id, 5.00d, "USD")
+
+        when:
+            def response = productService.getProduct(id)
+
+        then:
+            1 * redSky.getTitle(id) >> "Avatar"
+            1 * priceRepository.findById(id) >> new Optional<Price>(price)
+            response instanceof Product
+            response.getId() == id
+            response.getName() == "Avatar"
+            response.getCurrentPrice() == price
+    }
+}
